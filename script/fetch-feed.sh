62c0eabeee032838314420a0b2a266cbde729ed0
oliverl-21 <77393143+oliverl-21@users.noreply.github.com>
2026-02-20 16:40:43 +0100

    fixed

--- script/fetch-feed.sh
+++ script/fetch-feed.sh
@@ -2,6 +2,6 @@
 countryfeed=("DE" "GR" "GB" "BE" "FR" "TR" "NO" "AT" "RO" "CZ" "SE" "NL" "FI" "SK" "UA" "ES" "MK" "IT" "CH" "PL" "BG" "LV" "DK")
 
 for feed in "${countryfeed[@]}"; do
-  curl -fsSL 'https://mirrors.rockylinux.org/mirrorlist?arch=x86_64&repo=BaseOS-10&country='$feed | awk -F/ '/:\/\//{print $3}' | sed 's/:.*//' | sort -u > feed/BaseOS/$feed
+  curl -fsSL 'https://mirrors.rockylinux.org/mirrorlist?arch=x86_64&repo=BaseOS-10&country='$feed | awk -F/ '/:\/\//{print $3}' | sed 's/:.*//' | sort -u > feed/BaseOS/Europe-$feed
 done
 
